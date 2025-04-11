2ffb76fe8f2647d7d36ae0c88fdcd7f9d6e2ead7
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2025-04-11 14:54:04 +0100

    initial commit

---
+++ site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $name = 'Christian';
+    $age= 60;
+    $gdp = 4.6;
+    // $datatype = var_dump($name);
+    echo ('<h1>Hello world in PHP</h1>');
+    echo "Datatype is " . var_dump($name);
+    ?>
+</body>
+</html>
